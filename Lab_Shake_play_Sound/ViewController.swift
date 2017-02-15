d8ae9dac8904c6ec7674b23980818e8ff381f33c
Gai lowvapong <mumu@gais-mac.local>
2017-02-15 03:13:15 -0800

    Add work

--- Lab_Shake_play_Sound/ViewController.swift
+++ Lab_Shake_play_Sound/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+     var player = AVAudioPlayer()
+     var audiopath = ""
+     var soundname = ["boing","explosion","hit","knife","shoot","swish","wah","warble"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+	
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        if event?.subtype == UIEventSubtype.motionShake {
+            
+            
+            let rannum = Int(arc4random_uniform(UInt32(soundname.count)))
+            
+            
+            audiopath = Bundle.main.path(forResource: String(soundname[rannum]), ofType: "mp3")!
+            
+            
+            do{
+                try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audiopath))
+            }
+            catch{
+                print("error")
+            }
+            
+            
+            player.play()
+            print(rannum)
+            print(soundname[rannum])
+        }
+    }
+    
 }
 
